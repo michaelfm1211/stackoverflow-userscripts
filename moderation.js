e775833874ded2a5d7a75b46a4659459852f9371
Michael M <42911957+michaelfm1211@users.noreply.github.com>
2022-12-26 11:42:38 -0600

    Add many fast-actions
    Added tag-spam, java is not javascript, and cv as debug fast-actions

--- moderation.js
+++ moderation.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         michaelfm1211's Moderation Fast Actions
 // @namespace    https://github.com/michaelfm1211/stackoverflow-userscripts
-// @version      0.1
+// @version      1.0
 // @description  Faster actions for common moderation tasks.
 // @author       Michael M
 // @match        https://stackoverflow.com/questions/*
@@ -14,6 +14,7 @@
 ///
 
 const privs = {
+    flag: 15,
     comment: 500,
     dv: 125,
     edit: 2000,
@@ -41,71 +42,119 @@ function waitForEl(sel) {
     });
 }
 
-// Create container for the comment actions
-function create_comments_container() {
-    const addComment = document.querySelector('.comments-link');
-
+// Create bar for the comment actions
+function create_comments_bar() {
     const container = document.createElement('span');
     container.style.float = 'right';
 
-    addComment.insertAdjacentElement('afterend', container);
+    document.querySelector('.comments-link').insertAdjacentElement('afterend', container);
     return container;
+}
+
+// Create close reasons bar
+function create_reasons_bar() {
+    const bar = document.createElement('div');
+    bar.className = 'd-flex gs8 s-anchors s-anchors_muted fw-wrap';
+    document.querySelector('.js-post-menu>div').insertAdjacentElement('afterend', bar);
+    return bar;
+}
+
+// Add a fast-action button
+function create_fast_action(bar, name, callback) {
+    // Create button
+    const outer = document.createElement('span');
+    outer.appendChild(document.createTextNode('['));
+
+    const ioc = document.createElement('a');
+    ioc.classList.add('comments-link');
+    ioc.textContent = name;
+    // Button event listeners
+    ioc.addEventListener('click', callback);
+
+    // Add button
+    outer.appendChild(ioc);
+    outer.appendChild(document.createTextNode(']'));
+    bar.appendChild(outer);
+}
+
+async function addComment(comment) {
+    if (!can(privs.comment)) return;
+    document.querySelector('.js-add-link').click();
+    await waitForEl('.js-comment-text-input');
+    const input = document.querySelector('.js-comment-text-input');
+    input.textContent = comment;
+}
+
+// Edit post and focus/scroll to tag bar
+async function tag_edit() {
+    document.querySelector('.js-edit-post').click();
+    await waitForEl('#tageditor-replacing-tagnames--input');
+    const tags = document.querySelector('#tageditor-replacing-tagnames--input');
+    tags.focus();
+    tags.scrollIntoView(); // does not work on longer posts. See issue #1
+}
+
+// Close vote or flag as 'Needs debugging details'
+async function cv_debug() {
+    if (!can(privs.flag) && !can(privs.cv)) return;
+    if (can(privs.flag) && !can(privs.cv)) {
+        document.querySelector('.js-flag-post-link').click();
+        await waitForEl('.js-flag-load-close');
+        document.querySelector('.js-flag-load-close').click();
+        await waitForEl('.js-close-question-link');
+    }
+
+    document.querySelector('.js-close-question-link').click();
+    await waitForEl('#closeReasonId-SiteSpecific');
+    document.querySelector('#closeReasonId-SiteSpecific').click();
+    await waitForEl('#siteSpecificCloseReasonId-13-');
+    document.querySelector('#siteSpecificCloseReasonId-13-').click();
 }
 
 ///
 // Fast Actions
 ///
 
-// Comment/close 'Image of Code' fast-action
-function image_of_code(container) {
-    // Create button
-    const outer = document.createElement('span');
-    outer.appendChild(document.createTextNode('['));
-
-    const ioc = document.createElement('a');
-    ioc.classList.add('js-add-link');
-    ioc.classList.add('comments-link');
-    ioc.textContent = 'Image of Code';
-
-    // Button event listeners
-    ioc.addEventListener('click', async () => {
-        if (can(privs.cv)) {
-            // Close vote for 'Needs debugging details'
-            document.querySelector('.js-close-question-link').click();
-            await waitForEl('#closeReasonId-SiteSpecific');
-            document.querySelector('#closeReasonId-SiteSpecific').click();
-            await waitForEl('#siteSpecificCloseReasonId-13-');
-            document.querySelector('#siteSpecificCloseReasonId-13-').click();
-        }
-
-        if (can(privs.comment)) {
-            // Open comments
-            await waitForEl('.js-comment-text-input');
-            const input = document.querySelector('.js-comment-text-input');
-            input.textContent = '[Please do not upload images of code/data/errors.](//meta.stackoverflow.com/q/285551)';
-        }
-    });
-
-    // Add button
-    outer.appendChild(ioc);
-    outer.appendChild(document.createTextNode(']'));
-    container.appendChild(outer);
-}
-
 // Edit tags fast action. Disabled for users with inline tag edit privs.
-function tag_edit() {
+function tag_edit_fa() {
     if (can(privs.tagEdit)) return;
     const btn = document.createElement('a');
     btn.textContent = 'Edit tags';
-    btn.addEventListener('click', async () => {
-        document.querySelector('.js-edit-post').click();
-        await waitForEl('#tageditor-replacing-tagnames--input');
-        const tags = document.querySelector('#tageditor-replacing-tagnames--input');
-        tags.focus();
-        tags.scrollIntoView(); // does not work on longer posts. See issue #1
-    });
+    btn.addEventListener('click', tag_edit);
 
     document.querySelector('.post-taglist>div>.js-post-tag-list-wrapper').appendChild(btn);
+}
+
+// Comment/close 'Image of Code' fast-action
+function image_of_code_fa(bar) {
+    create_fast_action(bar, 'Image of Code', async () => {
+        await cv_debug();
+        await addComment('[Please do not upload images of code/data/errors.](//meta.stackoverflow.com/q/285551)');
+    });
+}
+
+// Comment/retag 'Tag Spam' fast-action
+function tag_spam_fa(bar) {
+    create_fast_action(bar, 'Tag Spam', async () => {
+        tag_edit();
+        await addComment('Please only add tags that are relevant to your question and avoid tag spamming.');
+    });
+}
+
+// Comment/retag 'Tag Spam' fast-action
+function java_is_not_javascript_fa(bar) {
+    create_fast_action(bar, 'Java/JS', async () => {
+        tag_edit();
+        await addComment('[Java is not JavaScript!](http://javascriptisnotjava.com/)');
+    });
+}
+
+// Close as 'needs debugging details' fast-action
+function cv_debug_fa(bar) {
+    if (!can(privs.flag) && !can(privs.cv)) return;
+    create_fast_action(bar, 'Needs debug', async () => {
+        cv_debug();
+    });
 }
 
 ///
@@ -115,8 +164,17 @@ function tag_edit() {
 (function() {
     'use strict';
 
-    const comments_container = create_comments_container();
-    image_of_code(comments_container);
+    if (can(privs.comment)) {
+        const comments_bar = create_comments_bar();
+        image_of_code_fa(comments_bar);
+        tag_spam_fa(comments_bar);
+        java_is_not_javascript_fa(comments_bar);
+    }
 
-    tag_edit();
+    if (can(privs.flag) || can(privs.cv)) {
+        const reasons_bar = create_reasons_bar();
+        cv_debug_fa(reasons_bar);
+    }
+
+    tag_edit_fa();
 })();
